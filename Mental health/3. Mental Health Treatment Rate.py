0001818b6b4a756aa6d543a54dab5b78128ad936
nitz0010 <saikianitish4566@gmail.com>
2025-05-19 20:29:44 +0530

    Delete Mental health directory

--- Mental health/3. Mental Health Treatment Rate.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-
-df = pd.read_csv('Mental Health.csv')
-df['seeks_treatment'].value_counts().plot(kind='pie', autopct='%1.1f%%', startangle=90)
-plt.title("Treatment for Mental Health")
-plt.ylabel('')
-plt.tight_layout()
-plt.show()
